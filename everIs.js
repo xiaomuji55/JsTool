d4e99c43cc63dc1f76a4cb2b5145177225a73bb1
xiaomuji_gz <1942018083@qq.com>
2018-11-20 16:33:42 +0800

    散出is 方法

--- everIs.js
+++ everIs.js
@@ -2,18 +2,6 @@
     'use strict'
     var EverIs = {
 
-        /**
-         * 说明：判断是否是数组
-         * 参数： 研判的的变量
-         * 返回： true或false
-         */
-        isArray: function(arr){
-            if (typeof Array.isArray === "function") {
-                return Array.isArray(value);
-            }else{
-                return Object.prototype.toString.call(value) === "[object Array]";
-            }
-        }
   
     }
 
